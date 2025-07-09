7a9ba9a3e1b9b12174514e579c64e254392ca475
Anselmo Alexandre <anselmo@snappmobile.io>
2025-07-09 17:16:46 +0200

    DataStore module setip

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -23,10 +23,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
+    implementation(libs.bundles.datastore)
+
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- core/datastore/src/main/kotlin/com/anselmoalexandre/datastore/AuthorAuth.kt
+++ core/datastore/src/main/kotlin/com/anselmoalexandre/datastore/AuthorAuth.kt
@@ -1,5 +1,37 @@
 package com.anselmoalexandre.datastore
 
-class AuthorAuth {
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 
+class AuthorAuth(private val dataStore: DataStore<Preferences>) {
+    private object PreferenceKeys {
+        val IS_AUTHENTICATED = booleanPreferencesKey("has-authenticated")
+    }
+
+    val isAuthenticated: Flow<Boolean> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else throw exception
+        }
+        .map { preferences ->
+            preferences[PreferenceKeys.IS_AUTHENTICATED] ?: false
+        }
+
+    suspend fun storeAuth(auth: Boolean = false) {
+        dataStore.edit { preferences ->
+            preferences[PreferenceKeys.IS_AUTHENTICATED] = auth
+        }
+    }
+
+    companion object {
+        const val AUTHENTICATED_KEY = "pref"
+    }
 }
